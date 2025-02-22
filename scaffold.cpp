a1c518e279c2ee89ca3fa0dfa076e41c596273ff
Havin <43862699+havindh@users.noreply.github.com>
2025-02-21 16:46:03 -0800

    Changed the scaffold.cpp to be able to call the mathfunc function

--- scaffold.cpp
+++ scaffold.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <mathfunc.h>
 using namespace std;
 
 
@@ -8,19 +8,29 @@ int main(int argc, char *argv[]) {
     const string EXIT = "quit";
     string command;
     do {
-	cout << "calc: ";
+      	cout << "calc: ";
         cin >> command;
 
         if ("help" == command)
         {
             // to do
-	    cout << "help documentation" << endl;
+	          cout << "help documentation" << endl;
+        }
+        else if (command == "math"){
+          int num1;
+          int num2;
+          string op;
+          cout << "Enter two numbers: ";
+          cin >> num1 >> num2 ;
+          cout << "Enter add, sub, mult, or div ";
+          cin >> op;
+          math_func(num1,num2,op);
         }
         else
         {
            // to do for start
-	   cout << "unknown command" << endl;
-	   return 1;
+	         cout << "unknown command" << endl;
+	         return 1;
         }
     } while (EXIT != command);
     return 0;
